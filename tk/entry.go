81c172d04fb2f431ec739c192c371547e352c737
xttk <3064668872@qq.com>
2022-05-22 21:56:38 +0800

    简化Entry

--- tk/entry.go
+++ tk/entry.go
@@ -40,92 +40,102 @@ func (w *Entry) Attach(id string) error {
 	return nil
 }
 
-func (w *Entry) SetForeground(color string) error {
-	setObjText("atk_tmp_text", color)
-	return eval(fmt.Sprintf("%v configure -foreground $atk_tmp_text", w.id))
-}
+// var useTkWidget = tk.WidgetAttrInitUseTheme(false)
+// tk.NewEntry(parent Widget) ==> ttk.Entry
+// tk.NewEntry(parent Widget, useTkWidget) ==> tk.Entry
 
-func (w *Entry) Foreground() string {
-	r, _ := evalAsString(fmt.Sprintf("%v cget -foreground", w.id))
-	return r
-}
+// w.ConfigSet(key string, value string) error
+// w.ConfigGet(key string) string
+// ttk key :
+// tk key : 
 
-func (w *Entry) SetBackground(color string) error {
-	setObjText("atk_tmp_text", color)
-	return eval(fmt.Sprintf("%v configure -background $atk_tmp_text", w.id))
-}
+// 以下设置和属性获取使用ConfigSet、ConfigGet
+// func (w *Entry) SetForeground(color string) error {
+// 	setObjText("atk_tmp_text", color)
+// 	return eval(fmt.Sprintf("%v configure -foreground $atk_tmp_text", w.id))
+// }
 
-func (w *Entry) Background() string {
-	r, _ := evalAsString(fmt.Sprintf("%v cget -background", w.id))
-	return r
-}
+// func (w *Entry) Foreground() string {
+// 	r, _ := evalAsString(fmt.Sprintf("%v cget -foreground", w.id))
+// 	return r
+// }
 
-func (w *Entry) SetWidth(width int) error {
-	return eval(fmt.Sprintf("%v configure -width {%v}", w.id, width))
-}
+// func (w *Entry) SetBackground(color string) error {
+// 	setObjText("atk_tmp_text", color)
+// 	return eval(fmt.Sprintf("%v configure -background $atk_tmp_text", w.id))
+// }
 
-func (w *Entry) Width() int {
-	r, _ := evalAsInt(fmt.Sprintf("%v cget -width", w.id))
-	return r
-}
+// func (w *Entry) Background() string {
+// 	r, _ := evalAsString(fmt.Sprintf("%v cget -background", w.id))
+// 	return r
+// }
 
-func (w *Entry) SetFont(font Font) error {
-	if font == nil {
-		return ErrInvalid
-	}
-	return eval(fmt.Sprintf("%v configure -font {%v}", w.id, font.Id()))
-}
+// func (w *Entry) SetWidth(width int) error {
+// 	return eval(fmt.Sprintf("%v configure -width {%v}", w.id, width))
+// }
 
-func (w *Entry) Font() Font {
-	r, err := evalAsString(fmt.Sprintf("%v cget -font", w.id))
-	return parserFontResult(r, err)
-}
+// func (w *Entry) Width() int {
+// 	r, _ := evalAsInt(fmt.Sprintf("%v cget -width", w.id))
+// 	return r
+// }
 
-func (w *Entry) SetJustify(justify Justify) error {
-	return eval(fmt.Sprintf("%v configure -justify {%v}", w.id, justify))
-}
+// func (w *Entry) SetFont(font Font) error {
+// 	if font == nil {
+// 		return ErrInvalid
+// 	}
+// 	return eval(fmt.Sprintf("%v configure -font {%v}", w.id, font.Id()))
+// }
 
-func (w *Entry) Justify() Justify {
-	r, err := evalAsString(fmt.Sprintf("%v cget -justify", w.id))
-	return parserJustifyResult(r, err)
-}
+// func (w *Entry) Font() Font {
+// 	r, err := evalAsString(fmt.Sprintf("%v cget -font", w.id))
+// 	return parserFontResult(r, err)
+// }
 
-func (w *Entry) SetShow(show string) error {
-	setObjText("atk_tmp_text", show)
-	return eval(fmt.Sprintf("%v configure -show $atk_tmp_text", w.id))
-}
+// func (w *Entry) SetJustify(justify Justify) error {
+// 	return eval(fmt.Sprintf("%v configure -justify {%v}", w.id, justify))
+// }
 
-func (w *Entry) Show() string {
-	r, _ := evalAsString(fmt.Sprintf("%v cget -show", w.id))
-	return r
-}
+// func (w *Entry) Justify() Justify {
+// 	r, err := evalAsString(fmt.Sprintf("%v cget -justify", w.id))
+// 	return parserJustifyResult(r, err)
+// }
 
-func (w *Entry) SetState(state State) error {
-	return eval(fmt.Sprintf("%v configure -state {%v}", w.id, state))
-}
+// func (w *Entry) SetShow(show string) error {
+// 	setObjText("atk_tmp_text", show)
+// 	return eval(fmt.Sprintf("%v configure -show $atk_tmp_text", w.id))
+// }
 
-func (w *Entry) State() State {
-	r, err := evalAsString(fmt.Sprintf("%v cget -state", w.id))
-	return parserStateResult(r, err)
-}
+// func (w *Entry) Show() string {
+// 	r, _ := evalAsString(fmt.Sprintf("%v cget -show", w.id))
+// 	return r
+// }
 
-func (w *Entry) SetTakeFocus(takefocus bool) error {
-	return eval(fmt.Sprintf("%v configure -takefocus {%v}", w.id, boolToInt(takefocus)))
-}
+// func (w *Entry) SetState(state State) error {
+// 	return eval(fmt.Sprintf("%v configure -state {%v}", w.id, state))
+// }
 
-func (w *Entry) IsTakeFocus() bool {
-	r, _ := evalAsBool(fmt.Sprintf("%v cget -takefocus", w.id))
-	return r
-}
+// func (w *Entry) State() State {
+// 	r, err := evalAsString(fmt.Sprintf("%v cget -state", w.id))
+// 	return parserStateResult(r, err)
+// }
 
-func (w *Entry) SetExportSelection(export bool) error {
-	return eval(fmt.Sprintf("%v configure -exportselection {%v}", w.id, boolToInt(export)))
-}
+// func (w *Entry) SetTakeFocus(takefocus bool) error {
+// 	return eval(fmt.Sprintf("%v configure -takefocus {%v}", w.id, boolToInt(takefocus)))
+// }
 
-func (w *Entry) IsExportSelection() bool {
-	r, _ := evalAsBool(fmt.Sprintf("%v cget -exportselection", w.id))
-	return r
-}
+// func (w *Entry) IsTakeFocus() bool {
+// 	r, _ := evalAsBool(fmt.Sprintf("%v cget -takefocus", w.id))
+// 	return r
+// }
+
+// func (w *Entry) SetExportSelection(export bool) error {
+// 	return eval(fmt.Sprintf("%v configure -exportselection {%v}", w.id, boolToInt(export)))
+// }
+
+// func (w *Entry) IsExportSelection() bool {
+// 	r, _ := evalAsBool(fmt.Sprintf("%v cget -exportselection", w.id))
+// 	return r
+// }
 
 func (w *Entry) SetText(text string) error {
 	setObjText("atk_tmp_text", text)
@@ -271,41 +281,42 @@ func (w *Entry) SelectionEnd() int {
 	return w.index("sel.last")
 }
 
-func EntryAttrForeground(color string) *WidgetAttr {
-	return &WidgetAttr{"foreground", color}
-}
+// 简化
+// func EntryAttrForeground(color string) *WidgetAttr {
+// 	return &WidgetAttr{"foreground", color}
+// }
 
-func EntryAttrBackground(color string) *WidgetAttr {
-	return &WidgetAttr{"background", color}
-}
+// func EntryAttrBackground(color string) *WidgetAttr {
+// 	return &WidgetAttr{"background", color}
+// }
 
-func EntryAttrWidth(width int) *WidgetAttr {
-	return &WidgetAttr{"width", width}
-}
+// func EntryAttrWidth(width int) *WidgetAttr {
+// 	return &WidgetAttr{"width", width}
+// }
 
-func EntryAttrFont(font Font) *WidgetAttr {
-	if font == nil {
-		return nil
-	}
-	return &WidgetAttr{"font", font.Id()}
-}
+// func EntryAttrFont(font Font) *WidgetAttr {
+// 	if font == nil {
+// 		return nil
+// 	}
+// 	return &WidgetAttr{"font", font.Id()}
+// }
 
-func EntryAttrJustify(justify Justify) *WidgetAttr {
-	return &WidgetAttr{"justify", justify}
-}
+// func EntryAttrJustify(justify Justify) *WidgetAttr {
+// 	return &WidgetAttr{"justify", justify}
+// }
 
-func EntryAttrShow(show string) *WidgetAttr {
-	return &WidgetAttr{"show", show}
-}
+// func EntryAttrShow(show string) *WidgetAttr {
+// 	return &WidgetAttr{"show", show}
+// }
 
-func EntryAttrState(state State) *WidgetAttr {
-	return &WidgetAttr{"state", state}
-}
+// func EntryAttrState(state State) *WidgetAttr {
+// 	return &WidgetAttr{"state", state}
+// }
 
-func EntryAttrTakeFocus(takefocus bool) *WidgetAttr {
-	return &WidgetAttr{"takefocus", boolToInt(takefocus)}
-}
+// func EntryAttrTakeFocus(takefocus bool) *WidgetAttr {
+// 	return &WidgetAttr{"takefocus", boolToInt(takefocus)}
+// }
 
-func EntryAttrExportSelection(export bool) *WidgetAttr {
-	return &WidgetAttr{"exportselection", boolToInt(export)}
-}
+// func EntryAttrExportSelection(export bool) *WidgetAttr {
+// 	return &WidgetAttr{"exportselection", boolToInt(export)}
+// }
